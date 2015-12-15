d2b4b3d0735c6a9ce76fe3eca23d14ac0335874a
Kevin <arcegk@gmail.com>
2015-12-14 22:11:52 -0500

    checkrange problems

--- rest/admin.py
+++ rest/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Plato , User ,Menu , Pedido , Config
+from .models import Plato , User ,Menu , Pedido , Config , Ubicacion
 
 
 admin.site.register(Plato)
 admin.site.register(User)
 admin.site.register(Pedido)
 admin.site.register(Config)
+admin.site.register(Ubicacion)
