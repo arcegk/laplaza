1a61bd5c52ac94e69f451f610ac1f666536cb638
Kevin <arcegk@gmail.com>
2016-02-17 12:15:49 -0500

    debug -> ture

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -24,7 +24,7 @@ env = environ.Env()
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = True
 
